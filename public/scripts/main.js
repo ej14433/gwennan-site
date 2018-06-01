1653c1d1bfbe6c9f7bc5c2ab6dbdb65857e87bff
Elis Jones <ej14433@my.bristol.ac.uk>
2018-06-01 11:16:00 +0100

    Added text and about

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -57,6 +57,7 @@ function selectProject(target) {
 }
 
 function changeContent(target) {
+	const about = document.querySelector('.about-content');
 	const showcase = document.querySelector('.showcase');
 	const body = document.querySelector('body');
 	const imgs = document.querySelectorAll('.showcase-img');
@@ -64,6 +65,7 @@ function changeContent(target) {
 	let imagesSrcArray = [];
 
 	if (target.classList.contains('atob')) {
+		about.style.display = 'none';
 		desc.innerHTML = `Growing up in the Welsh country Gwennan found herself able to navigate around without the help of road signs, to be dropped in the middle of nowhere, and knowing where she was based on the landscape's familiarities. This is what the pattern portrays, but not through the country side but rather Gwennan’s walk to University; from Peckham to Camberwell. Every square represents a different checkpoint in the landmarks on her route. Using these checkpoints as an indicator of how long I have left to walk.`;
 		showcase.style.display = 'grid';
 		body.style.backgroundColor = 'rgb(244,113,193)';
@@ -73,6 +75,7 @@ function changeContent(target) {
 			'image-to-go-in-AIB.jpg'
 		];
 	} else if (target.classList.contains('homage')) {
+		about.style.display = 'none';
 		desc.innerHTML = `Homage was a commentary piece on Gwennan's identity. She portrayed her torn feelings about her connection with Wales by merging her roots and her life now in London. Each ceramic symbolises different elements that she has taken with her from Wales to each of her houses in London, such as the importance of tea and the communal area. As a twist each ceramics have a design flaw, they cannot be used for their visual function. By displaying the ceramic pieces on the traditional Welsh dresser that has been in her family for generations evokes the closeness she feels towards Wales but also her displacement within.`;
 		showcase.style.display = 'grid';
 		body.style.backgroundColor = 'rgb(236,209,69)';
@@ -82,6 +85,7 @@ function changeContent(target) {
 			'image-of-the-right.jpg'
 		];
 	} else if (target.classList.contains('community-garden')) {
+		about.style.display = 'none';
 		desc.innerHTML = `Community Garden was a collaborative project with Surface Designer & Illustrator Charlotte Cropper. The collaboration sparked from their shared joy of colour and patterns. They created a starter pack for local garden allotments around Camberwell to encourage home growing and less waste. Using Beetroot as the main focus of design both creatives designed accessible and durable gardening tools along with a welcome and a how to guide.`;
 		showcase.style.display = 'grid';
 		body.style.backgroundColor = 'rgb(76,100,198)';
@@ -91,6 +95,7 @@ function changeContent(target) {
 			'table-top-real-size.jpg'
 		];
 	} else if (target.classList.contains('about')) {
+		about.style.display = 'grid';
 		body.style.backgroundColor = 'rgb(235,80,163)';
 		showcase.style.display = 'none';
 		desc.innerHTML = '';
